487cf0c603816f7a1178813c81e67b92e42e373d
rtre84 <rtre84@users.noreply.github.com>
2020-09-23 23:27:19 -0400

    Testing with fixer api.

--- exchangerates/app.py
+++ exchangerates/app.py
@@ -263,6 +263,83 @@ async def index(request):
     return await file("./exchangerates/templates/index.html")
 
 
+@app.route("/fixer/latest", methods=["GET", "HEAD"])
+@app.route("/fixer/<date>", methods=["GET", "HEAD"])
+@app.route("/fixer/api/latest", methods=["GET", "HEAD"])
+@app.route("/fixer/api/<date>", methods=["GET", "HEAD"])
+@cors()
+async def exchange_rates(request, date=None):
+    if request.method == "HEAD":
+        return json("")
+
+    dt = datetime.now()
+    if date:
+        try:
+            dt = datetime.strptime(date, "%Y-%m-%d")
+        except ValueError as e:
+            return json({"error": "{}".format(e)}, status=400)
+
+        if dt < datetime(1999, 1, 4):
+            return json(
+                {"error": "There is no data for dates older then 1999-01-04."},
+                status=400,
+            )
+
+    exchange_rates = (
+        await ExchangeRates.query.where(ExchangeRates.date <= dt.date())
+        .order_by(ExchangeRates.date.desc())
+        .gino.first()
+    )
+    rates = exchange_rates.rates
+
+    # Base
+    base = "EUR"
+    if "base" in request.raw_args and request.raw_args["base"] != "EUR":
+        base = request.raw_args["base"]
+
+        if base in rates:
+            base_rate = Decimal(rates[base])
+            rates = {
+                currency: Decimal(rate) / base_rate for currency, rate in rates.items()
+            }
+            rates["EUR"] = Decimal(1) / base_rate
+        else:
+            return json(
+                {"error": "Base '{}' is not supported.".format(base)}, status=400
+            )
+
+    # Symbols
+    if "symbols" in request.args:
+        symbols = list(
+            itertools.chain.from_iterable(
+                [symbol.split(",") for symbol in request.args["symbols"]]
+            )
+        )
+
+        if all(symbol in rates for symbol in symbols):
+            rates = {symbol: rates[symbol] for symbol in symbols}
+        else:
+            return json(
+                {
+                    "error": "Symbols '{}' are invalid for date {}.".format(
+                        ",".join(symbols), dt.date()
+                    )
+                },
+                status=400,
+            )
+
+    return json(
+        {"base": base, "date": exchange_rates.date.strftime("%Y-%m-%d"), "rates": rates}
+    )
+
+
+# Fixer.io
+@app.route("/fixer", methods=["GET"], host="api.exchangeratesapi.io")
+async def index(request):
+    access_key = getenv("ACCESS_KEY")
+    return json({"details": "https://data.fixer.io/api/latest?access_key="+access_key}, escape_forward_slashes=False)
+
+
 # Static content
 app.static("/static", "./exchangerates/static")
 app.static("/robots.txt", "./exchangerates/static/robots.txt")
