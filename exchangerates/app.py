5c3f9c804c93b7de576031c6b47e893c46c7e4d9
rtre84 <rtre84@users.noreply.github.com>
2021-01-21 17:00:13 +0530

    Revert "Graph route is being blocked. Attempting to open it back up."
    This reverts commit 7d3caef7af66ff7e3ed5eab8496756739969f988.

--- exchangerates/app.py
+++ exchangerates/app.py
@@ -31,7 +31,7 @@ app.config.update(
         url=getenv("DATABASE_URL", "postgresql://localhost/exchangerates")
     )
 )
-CORS(app)
+cors = CORS(app, resources={r"/graph": {"origins": "*"}})
 
 
 # Database
@@ -290,7 +290,7 @@ async def past_trend(request):
 #
 # This route should return preferably a cached version of the graph if it exists
 # or a version from cache
-@app.route("/graph", methods=["GET", "OPTIONS"])
+@app.route("/graph", methods=["GET"])
 async def graph(request):
     scraper_api_key = getenv("SCRAPER_API_KEY") if getenv("SCRAPER_API_KEY") else None
 
